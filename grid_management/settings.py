d4a0f93aa7be27df18b3bd598fa8fc0cd797c4d0
neohary <neohary@foxmail.com>
2022-10-06 16:15:35 +0800

    change

--- grid_management/settings.py
+++ grid_management/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wb*8adc^8@m0-@9=-gm!-jlq92fz52(xsa#v8drotbize*@&63'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['fierce-earth-92280.herokuapp.com','127.0.0.1']
 
 '''LOGGING = {
     'version': 1,
